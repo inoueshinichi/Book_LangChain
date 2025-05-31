c3dc74052585fdaa74141dedd613fe7321650e02
Macbook Air M4 <inoue.shinichi.1800@gmail.com>
2025-05-31 17:32:23 +0900

    [U] SystemMessage

--- langchain/02_model_io/01_language_model.py
+++ langchain/02_model_io/01_language_model.py
@@ -1,5 +1,6 @@
 """ Model I/O Language
 """
+from re import M
 import sys
 import os
 from pathlib import Path
@@ -31,7 +32,11 @@ logger.setLevel(logging.DEBUG)
 from dotenv import load_dotenv
 # from openai import OpenAI
 from langchain_community.chat_models import ChatOpenAI  #← モジュールをインポート
-from langchain.schema import HumanMessage 
+from langchain.schema import (
+    HumanMessage,
+    AIMessage,
+    SystemMessage,
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,7 +80,73 @@ def language_case1():
     except Exception as e:
         logger.error(e)
 
-    
+def language_case2():
+    logger.info(f"[Execute] {inspect.currentframe().f_code.co_name}")
+
+    OPENAI_SECRET_KEY = os.getenv('OPENAI_API_TOKEN')
+    if OPENAI_SECRET_KEY is None:
+        raise Exception('OpenAIのシークレットキーがNoneです')
+
+    try:
+        # AIMessageを使って言語モデルからの返答を表す
+        num_content = 1
+
+        chat = ChatOpenAI(
+            api_key=OPENAI_SECRET_KEY,
+            verbose=True,
+            model="gpt-4",
+            temperature=0.7,
+            # max_tokens=100,
+            n=num_content,
+        )
+
+        result = chat(messages=[
+            HumanMessage(content="茶碗蒸しの作り方を教えて"),
+            AIMessage(content="{ChatModelからの返答である茶碗蒸しの作り方}"),
+            HumanMessage(content="英語に翻訳して")
+        ])
+
+        logger.info(result)
+        print('---------[response message]-----------')
+        logger.info(result.content)
+
+    except Exception as e:
+        logger.error(e)
+
+
+def language_case3():
+    logger.info(f"[Execute] {inspect.currentframe().f_code.co_name}")
+
+    OPENAI_SECRET_KEY = os.getenv('OPENAI_API_TOKEN')
+    if OPENAI_SECRET_KEY is None:
+        raise Exception('OpenAIのシークレットキーがNoneです')
+
+    try:
+        # AIMessageを使って言語モデルからの返答を表す
+        num_content = 1
+
+        chat = ChatOpenAI(
+            api_key=OPENAI_SECRET_KEY,
+            verbose=True,
+            model="gpt-4",
+            temperature=0.7,
+            # max_tokens=100,
+            n=num_content,
+        )
+
+        result = chat(messages=[
+            SystemMessage(content="あなたは親しい友人です。返答は敬語は使わず、フランクに会話してください。"),
+            HumanMessage(content="やあ！ TinyTankだよ。こんにちは!")
+        ])
+
+        logger.info(result)
+        print('---------[response message]-----------')
+        logger.info(result.content)
+
+    except Exception as e:
+        logger.error(e)
     
 if __name__ == "__main__":
-    language_case1()
+    # language_case1()
+    # language_case2()
+    language_case3()
